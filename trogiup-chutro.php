4704a126f70db83b4e6eddee6adc052ad7539ac0
kimngan2505 <nganb1906328@student.ctu.edu.vn>
2023-03-19 22:56:07 +0700

    trogiup_chutro
    Hướng dẫn và quy định đăng bài

---
+++ trogiup-chutro.php
@@ -0,0 +1,255 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+        <meta http-equiv="Content-Type" content="text/php; charset=utf-8" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+        <script src="js/js.js" type="text/javascript"></script>
+        <link rel="shortcut icon" href="image/home-v1.png"/>
+        <link href="css/trogiup.css" rel="stylesheet" type="text/css" media="all"/>
+        <link href="css/trogiup_chutro.css" rel="stylesheet" type="text/css" media="all"/>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <meta name="theme-color" content="#FFBA00"/>
+        <meta name="full-screen" content="yes"/>
+        <meta name="apple-mobile-web-app-status-bar-style" content="#FFBA00"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Satisfy&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Saira+Semi+Condensed:wght@200&family=Satisfy&display=swap" rel="stylesheet">
+        <link rel="preload" as="font" crossorigin="anonymous" data-href="https://fonts.googleapis.com/css?family=Helvetica&amp;display=swap"/>
+        <meta charSet="utf-8"/>
+        <title>Trợ Giúp</title>
+        <meta name="description" content="BingHouse - Website đăng,tim nhà trọ Cần Thơ mỗi ngày. Đăng tin mua bán UY TÍN, NHANH CHÓNG, AN TOÀN."/>
+        <meta property="og:type" content="website"/>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="js/ajax.js" type="text/javascript"></script>
+      
+    </head>
+<body class="hero-anime">
+<div class="navigation-wrap bg-light start-header start-style">
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <nav class="navbar navbar-expand-md navbar-light">
+       
+        <div class="ssn_header" alt="">
+        <a class="ssn_logo" title="binghouse" href="index.php">
+            <img src="image/logo.png" alt="BingHouse">
+            <!-- <img src="image/desiglogo.png" alt="BingHouse"> -->
+            <a class="logo_text"> BingHouse </a>
+        </a>
+    </div>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto py-4 py-md-0">
+              <li class="nav-item pl-4 pl-md-2 ml-0 ml-md-4 active">
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Home</a>
+                
+              </li>
+              <li class="nav-item pl-4 pl-md-2 ml-0 ml-md-4">
+                <a class="nav-link" href="#">Tìm thuê</a>
+              </li>
+              <li class="nav-item pl-4 pl-md-2 ml-0 ml-md-4">
+                <a class="nav-link" href="#">Quản lý tin</a>
+              </li>
+              <li class="nav-item pl-4 pl-md-2 ml-0 ml-md-4">
+                <a class="nav-link" href="#">Thông báo</a>
+              </li>
+              <li class="nav-item pl-4 pl-md-2 ml-0 ml-md-4">
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" class="aw__i1t7n4kh aw__i1t7n4kh--active"><path fill-rule="evenodd" clip-rule="evenodd" d="M9.99935 3.08366C6.17938 3.08366 3.08268 6.18036 3.08268 10.0003C3.08268 13.8203 6.17938 16.917 9.99935 16.917C13.8193 16.917 16.916 13.8203 16.916 10.0003C16.916 6.18036 13.8193 3.08366 9.99935 3.08366ZM1.91602 10.0003C1.91602 5.53602 5.53505 1.91699 9.99935 1.91699C14.4637 1.91699 18.0827 5.53602 18.0827 10.0003C18.0827 14.4646 14.4637 18.0837 9.99935 18.0837C5.53505 18.0837 1.91602 14.4646 1.91602 10.0003Z" fill="#222222"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M9.99935 6.83366C8.59562 6.83366 7.45768 7.9716 7.45768 9.37533C7.45768 10.779 8.59562 11.917 9.99935 11.917C11.4031 11.917 12.541 10.779 12.541 9.37533C12.541 7.9716 11.4031 6.83366 9.99935 6.83366ZM6.29102 9.37533C6.29102 7.32727 7.95129 5.66699 9.99935 5.66699C12.0474 5.66699 13.7077 7.32727 13.7077 9.37533C13.7077 11.4234 12.0474 13.0837 9.99935 13.0837C7.95129 13.0837 6.29102 11.4234 6.29102 9.37533Z" fill="#222222"></path><path fill-rule="evenodd" clip-rule="evenodd" d="M10.0005 13.0837C9.06941 13.0837 8.15655 13.3415 7.36306 13.8285C6.56957 14.3156 5.92644 15.0128 5.50496 15.843C5.35912 16.1303 5.00802 16.2449 4.72075 16.0991C4.43349 15.9532 4.31884 15.6021 4.46469 15.3149C4.98369 14.2926 5.77564 13.434 6.75275 12.8342C7.72986 12.2345 8.85396 11.917 10.0005 11.917C11.1469 11.917 12.271 12.2345 13.2481 12.8342C14.2253 13.434 15.0172 14.2926 15.5362 15.3149C15.6821 15.6021 15.5674 15.9532 15.2801 16.0991C14.9929 16.2449 14.6418 16.1303 14.4959 15.843C14.0745 15.0128 13.4313 14.3156 12.6378 13.8285C11.8444 13.3415 10.9315 13.0837 10.0005 13.0837Z" fill="#222222"></path></svg>
+               <a class="nav-link" href="#" data-toggle="dropdown">Tài khoản</a>
+               <svg data-toggle="dropdown" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="1em" height="1em" fill="none" class="aw__d1xmuhl0" id="arrowDownB"><path d="M7.9 156.8l2.8 3.3 214.8 247.2c7.3 8.4 18.2 13.6 30.3 13.6 12.2 0 23.1-5.4 30.3-13.6l214.7-246.7 3.6-4.1c2.7-3.9 4.3-8.7 4.3-13.7 0-13.7-11.7-25-26.2-25h-453c-14.5 0-26.2 11.2-26.2 25 0 5.2 1.7 10.1 4.6 14z" fill="currentColor"></path></svg>
+               
+                <div class="dropdown-menu">
+                <div class="aw__m12exo7"><a href="login.php" rel="nofollow"><span class="aw__mdmk8my"></span><span class="aw__meaxp5j">Đăng nhập / Đăng ký</span>
+              </a><div class="aw__m1pkalbk"><span class="aw__m9yyskr"></span></div>
+              <div class="aw__c1n389kw"></div></div>
+                  <a class="dropdown-item" href="#">Tin đăng đã lưu</a>
+                  <a class="dropdown-item" href="#">Tìm kiếm đã lưu</a>
+                  <a class="dropdown-item" href="#">Đánh giá từ tôi</a>
+                  <a class="dropdown-item" href="#">Cài đặt</a>
+                  <a class="dropdown-item" href="#">Trợ giúp</a>
+                </div>
+                </li>
+               
+                  <!-- <a href="signup.php" class="btn btn-primary pl-0 pl-md-2 ml-0 ml-md-4 btn-lg active" role="button" aria-pressed="true">Sign Up</a> -->
+                    
+                    </ul>
+                </div>
+
+                </nav>
+            </div>
+            </div>
+        </div>
+        </div>
+
+        <!-- Test -->
+        <article class="the-article">
+<div class="article-main-content">
+<h2><i class="fas fa-file"></i><strong> HƯỚNG DẪN ĐĂNG TIN</strong></h2><hr>
+    <p><strong>🌐 Chào bạn! Sau đây là hướng dẫn sử dụng cho thành viên website BingHouse.com.</p>
+    <p>Nếu bạn chưa có tài khoản, hãy <a href="signup.php">Đăng ký tại đây</a> trước khi bắt đầu đăng tin mới.</p>
+    <p>Nếu đã có tài khoản, sau khi <a href="login.php">Đăng nhập</a> vào website, bạn bấm vào nút <a href="posts.php">ĐĂNG TIN</a> để bắt đầu.</p>
+    <p>Khi đăng tin các bạn đọc kỹ mô tả từng bước, nhập đầy đủ và chính xác nội dung cho tin đăng, đặc biệt là mục Giá và Diện tích. Những tin có nội dung hình ảnh rõ ràng, đầy đủ sẽ có tỉ lệ xem cao hơn 50%.</p>
+    <hr><h3><i class="material-icons">attachment</i> Lưu ý khi đăng tin:</h3>
+    <p>&#176; Điền đầy đủ các thông tin bắt buộc vào các ô nhập liệu trong phần đăng tin.</p>
+    <p>&#176; Phần giá cho thuê, vui lòng nhập chính xác 1 giá duy nhất (Không nhập giá từ ....đến ....) và chọn đúng đơn vị giá là triệu/tháng hoặc nghìn/tháng. Ví dụ bạn cho thuê 3 triệu/tháng thì bạn nhập đủ như sau 3000000 (1 số 3 và 6 số 0)</p>
+    <p>&#176; Diện tích nhập đúng 1 diện tích duy nhất (Không nhập diện tích từ ....đến ....)</p>
+    <p>&#176; Sau khi nhập đầy đủ các thông tin, bấm ĐĂNG TIN NGAY và chờ vài giây để tin bạn hiển thị trên website, nếu đăng tin thành công hệ thống sẽ báo bạn đã đăng tin thành công, nếu hệ thống cảnh báo màu đỏ, các ô chọn màu bị sai, vui lòng nhập lại cho chính xác và bấm ĐĂNG TIN NGAY lại.</p>
+
+    <hr><h3><i class="material-icons">attachment</i> Mẹo vặt đăng tin:</h3>
+    <p>&#8227; Hình ảnh đẹp, rõ ràng, chân thực và độc đáo</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Mô tả: Khi đầu tiên khách hàng click vào bài hình ảnh sẽ hiển thị ngay đầu tiên và sẽ
+                là phần đập vào mắt khách hàng đầu tiên. Chính vì vậy hình ảnh cần rõ nét, đa dạng
+                để khách hàng tham khảo, tang tỷ lệ chuyển đổi, ảnh "độc" là ảnh tự chụp nhưng vẫn
+                đảm bảo chân thực và sắc nét.</p>
+    <p>&#176; Đặc biệt, là không đính kèm logo củaa các trang đăng tin khác.</p>
+    <p>&#176; Các tin đăng có ảnh đẹp, ro ràng, sắc nét hay độc đáo sẽ được ưu tiên hiển thị lên đầu
+                như vậy lượng người xem tin sẽ cao hơn</p>
+    <p>&#8227; Mẫu hình đẹp:</p><img src="image/phong1.PNG"/>
+    <p>&#176; Hình ảnh sáng và rõ nét</p>
+    <p>&#176; Góc ảnh được chụp cân đối</p>
+    <p>&#176; Chụp tại chính căn phòng của khách hàng sẽ thuê không dùng ảnh mẫu</p>
+    
+    <p>&#8227; Giá bán đúng</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Mô tả: Giá bán hợp lý với thị trường sẽ thúc đẩy người mua click vào tin đăng của
+            bạn. Nếu giá bán khác với số thực và khi khách hàng gọi báo giá thì sẽ làm giảm uy
+            tín tin đăng của bạn đối với người mua.</p>
+    <p>&#176; Nên ghi số tiền trong phần mô tả khi đăng tin để tạo sự hấp dẫn với người xem</p>
+    <p>&#8227; Tiêu đề súc tích, thu hút</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Mô tả: Vì tiêu đề của BingHouse tối thiểu 30 ký tự, tối đa 90 ký tự, khách hàng có thể
+                tạo cho mình những tiêu đề thu hút để người mua bị thuyết phục nhưng vẫn đảm bảo
+                những thông tin cơ bản nếu khách hàng cần như “Hình thức: Cho thuê phòng,
+                Loại phòng: Phòng có gác hay không gác - Diện tích - Vị trí”. Sử dụng thêm các thông tin như:
+                tình trạng pháp lý, số người ở tối đa, số tầng, nội thất phòng (nếu có)... để
+                tạo ấn tượng với người mua.</p>
+    <p>&#8227; Nội dung đầy đủ thông tin</p>                
+    <p>&nbsp;&nbsp;&nbsp; &#187; Mô tả: Thông tin được người đăng nhập đầy đủ chính xác về địa chỉ, các đặc điểm
+        của loại phòng đó, nội thất, tiện nghi (gần trường, gần trung tâm, gần siêu thị),... sẽ đem đến cho người xem đầy đủ thông tin về
+        loại phòng đó, qua đó thúc đẩy việc người xem quyết định nhanh hơn, tỷ lệ chuyển đổi của
+        các tin đăng như vậy sẽ cao hơn.</p>
+
+
+    <hr><h2><i class="fas fa-file"></i><strong> QUY ĐỊNH ĐĂNG TIN</strong></h2><hr>
+    <p>&#8226; Tin đăng phải có ảnh/video phù hợp, rõ nét, không chứa SĐT liên hệ, không chứa logo của các website về phòng trọ khác, không chứa nội dung phản cảm, nội dung trái pháp luật</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Mô tả: Tin đăng có hình ảnh/video xấu, nhòe nhoẹt, vỡ hình, xem không rõ hình, Tin
+        đăng có hình ảnh/video chứa nội dung phản cảm, nội dung trái pháp luật, Tin đăng
+        có chứa hình ảnh/video với logo của các website về phòng trọ khác. BingHouse: Sẽ trả lại tin này và thông báo qua Email.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187;  Lý do: Những tin đăng có chứa hình ảnh không đúng quy định của BingHouse sẽ bị
+        loại bỏ để đảm bảo người xem website xem được các hình ảnh chân thực, phù hợp,
+        mô tả đúng đắn về các phòng trọ cho thuê. BingHouse khuyến khích việc đăng tin có chứa các
+        hình ảnh đẹp, sắc nét, các tin đăng như vậy sẽ được ưu tiên hiển thị lên đầu và nhận
+        được điểm rating cao bởi tính thu hút người xem, người thuê phòng.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Xử lý: Người đăng tin khi bị trả lại tin kiểu này cần chỉnh sửa lại ảnh/video tin đăng cho phù hợp, cần chọn lựa đăng ảnh/video rõ nét, chân thực.</p>
+
+    <hr><p>&#8226; Tin đăng có các thông tin quan trọng như Giá, Diện tích, Số phòng, Có phòng toilet riêng không, Có gác không, Địa chỉ phòng,...phải đồng nhất với phần mô tả.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Mô tả: Loại tin đăng khi người đăng nhập các thông số ở form đăng tin như Giá, Diện
+        tích, Số phòng, Có phòng toilet riêng không, Có gác không, Địa chỉ phòng,... một đằng, bên dưới phần
+        mô tả người đăng tin lại viết một mô tả khác với các thông số đã nhập liệu này.
+        BingHouse: Sẽ trả lại tin này và thông báo qua Email.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Lý do: Đây là loại tin rác không đúng sự thật gây nhầm lẫn cho người duyệt website
+            nên phải loại bỏ để bảo vệ người thuê phòng trọ khi xem trên website.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Xử lý: Người đăng tin cần chỉnh sửa lại tin đồng nhất về nội dung đăng tin.</p>
+
+    <hr><p>&#8226; Tin đăng phải có địa chỉ rõ ràng</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Mô tả: Loại tin đăng có địa chỉ không đúng, không cụ thể tên đường, quận huyện,
+        thành phố Hoặc địa chỉ ở phần nhập thông tin khi đăng tin một kiểu, dưới phần mô tả
+        lại viết một địa chỉ khác.
+        BingHouse: Sẽ trả lại tin này và thông báo qua Email.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Lý do: Những tin đăng có địa chỉ không rõ ràng như vậy gây nhầm lẫn nghiêm trọng
+        đối với người xem website do đó để bảo vệ quyền lợi cho người xem website,
+        BingHouse sẽ trả lại những tin như này khi kiểm duyệt tin.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Xử lý: Người đăng tin khi bị trả lại tin kiểu này cần chỉnh sửa lại địa chỉ tin đăng cho
+        đầy đủ tên đường phố/ quận/ huyện/ thành phố, cho đồng nhất với phần mô tả về khu trọ đó.
+
+    <hr><p>&#8226; Tin đăng phải có giá phù hợp</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Mô tả: Tin đăng có giá cho thuê linh tinh như 1 VNĐ, 20 VNĐ,...Giá cho thuê là giá
+        một phần của giá trị căn phòng để nhầm thu hút hay có ý lừa đảo.
+        BingHouse: Sẽ trả lại tin này và thông báo qua Email.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Lý do: Sosanhnha muốn loại bỏ tin rác về giá bảo vệ người xem.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Xử lý: Người đăng tin chỉ có thể chỉnh sửa lại tin với giá cả đúng, hợp lý để thu hút người xem.</p>
+
+    <hr><p>&#8226; Mỗi account, tin mới đăng không ợ c trùng với các tin đăng đã được duyệt trước đó</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Mô tả: Loại tin đăng trùng là tin có nội dung copy, trùng lặp với các tin đã được đăng
+        trước đó trong cùng 1 tài khoản như các tin giống nhau về cả giá, diện tích, địa chỉ,...
+        BingHouse: Sẽ trả lại tin này và thông báo qua Email.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Lý do: BingHouse muốn loại bỏ tin rác bảo vệ người xem, kiểu một tài khoản đăng
+        liên tiếp nhiều tin cùng 1 nội dung với mục đích để tin về phìng đó của họ xuất hiện
+        nhiều trên website -> điều này tạo ra sự khó chịu với người duyệt website và tạo ra lượng tin trùng lặp rác cho website.</p>
+    <p>&nbsp;&nbsp;&nbsp; &#187; Xử lý: Người đăng tin chỉ có thể chỉnhh sửa lại tin cũ đã đăng trước đó như giá cả hợp lý hơn để thu hút người xem.</p>
+
+
+    <hr><div class="center">
+        <h6 class="section-title">Bạn đang có phòng trọ / căn hộ cho thuê?</h6>
+        <p>Không phải lo tìm người cho thuê, phòng trống kéo dài</p>
+        <a class="btn red" style="padding: 10px 30px;" rel="nofollow" href="login.php">Đăng tin ngay</a></div><br><br><br>
+</article>
+
+        <section class="section section-support">
+            <div class="section-content clearfix">
+            <div class="support-bg">
+                </div>
+            <div class="list-support clearfix">
+            <div class="list-support-title">Liên hệ với chúng tôi nếu bạn cần hỗ trợ:</div>
+            <div class="support-item">
+                <span class="support-item-title">Hỗ trợ thanh toán</span>
+                <a rel="nofollow" href="tel:0917686101">Điện thoại: 0909111112</a>
+                <a rel="nofollow" target="_blank" href="https://zalo.me/0917686101">Zalo: 0917999101</a>
+            </div>
+            <div class="support-item">
+                <span class="support-item-title">Hỗ trợ đăng tin</span>
+                <a rel="nofollow" href="tel:0902657123">Điện thoại: 0902222123</a>
+                <a rel="nofollow" target="_blank" href="https://zalo.me/0902657123">Zalo: 0902888123</a>
+            </div>
+            <div class="support-item">
+                <span class="support-item-title">Hotline 24/7</span>
+                <a rel="nofollow" href="tel:0917686101">Điện thoại: 0917444101</a>
+                <a rel="nofollow" target="_blank" href="https://zalo.me/0917686101">Zalo: 0917666101</a>
+            </div>
+            <a class="btn btn-page-contact" rel="nofollow" href="/lien-he">Gửi liên hệ</a>
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+
+
+
+        <!-- test -->
+            <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+            <script language="javascript">
+            $(document).ready(function () {
+            $('#input-form-search').autocomplete({
+            serviceUrl: '/ajax/suggest?secret=45c09f3828245d941d0da984d0222cb2',
+            noCache: true,
+            autoSelectFirst: false,
+            deferRequestBy: 300,
+            minChars: 0,
+            onSelect: function (suggestion) {
+            url = suggestion.url || '';
+            if (url !== '') {
+            window.location.href = url;
+            }
+            }
+            });
+            });
+            </script>
+
+            <style>
+            .autocomplete-suggestions{
+            z-index: 10050000!important;
+            }
+            </style>
+        </div>
+    </body>
+</html>
